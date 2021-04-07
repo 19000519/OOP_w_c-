b2fea38572d3200119bdf6e66f419e015ebb49f3
Phemelo Phenyo Koswane <61431257+19000519@users.noreply.github.com>
2021-04-07 11:49:31 +0200

    Add files via upload

---
+++ w2 p1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int length, width;
+    float area;
+
+    cout << "enter the length and width";
+    cin >>length >> width;
+
+    area = length * width;
+     cout << "area = " <<area;
+    return 0;
+}
